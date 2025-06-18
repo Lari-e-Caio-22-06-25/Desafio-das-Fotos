e76e706feb44bd7222f9bdaefb633c62e4c6e973
Lari-e-Caio-22-06-25 <ferraz.caio.ed@gmail.com>
2025-06-18 03:28:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const frases = [
-  "A persistência é o caminho do êxito.",
-  "Faça o seu melhor, mas sempre com leveza.",
-  "A vida começa onde termina o medo.",
-  "Seja a mudança que você quer ver no mundo."
-];
+      "Capture um abraço inesperado.",
+      "Foto com alguém usando algo terracota.",
+      "Momento espontâneo na pista de dança.",
+      "Um brinde cheio de alegria!",
+      "A noiva de costas (vale correr!).",
+      "O convidado mais animado da festa.",
+      "Algo verde que não seja planta!"
+    ];
 
-const fraseElemento = document.getElementById('frase');
+    const fraseElemento = document.getElementById('frase');
+    let fraseSalva = localStorage.getItem('fraseEscolhida');
 
-// Verifica se já existe uma frase salva
-let fraseSalva = localStorage.getItem('fraseEscolhida');
+    if (!fraseSalva) {
+      const indiceAleatorio = Math.floor(Math.random() * frases.length);
+      fraseSalva = frases[indiceAleatorio];
+      localStorage.setItem('fraseEscolhida', fraseSalva);
+    }
 
-if (!fraseSalva) {
-  // Se não existe, sorteia e salva
-  const indiceAleatorio = Math.floor(Math.random() * frases.length);
-  fraseSalva = frases[indiceAleatorio];
-  localStorage.setItem('fraseEscolhida', fraseSalva);
-}
-
-fraseElemento.innerText = fraseSalva;
+    fraseElemento.innerText = fraseSalva;
